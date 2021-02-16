cdc4380780758c115fbe5d9e1b0c9c051cf73d7e
Eddy <EddyPlata@github.com>
2021-02-15 23:41:41 -0500

    Implementing Strategies UI

--- Fullstack_trading_app/main.py
+++ Fullstack_trading_app/main.py
@@ -2,6 +2,7 @@ import sqlite3, config
 #import alpaca_trade_api as tradeapi
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from datetime import date
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 # We'll want a dashboard route,
@@ -10,10 +11,30 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/")
 def index(request:Request):
     print(dir(request))
+    stock_filter = request.query_params.get('filter',False)
     connection = sqlite3.connect("app.db")
     connection.row_factory = sqlite3.Row
     cursor=connection.cursor()
-    cursor.execute(""" SELECT id,symbol,name FROM stock order by symbol """)
+
+
+    if stock_filter == 'new_closing_highs':
+        cursor.execute("""
+        SELECT * FROM (
+        SELECT symbol,name,stock_id,max(close),date
+        from stock_price join stock on stock.id = stock_price.stock_id
+        group by stock_id order by symbol
+        ) where date = ?
+        """, (date.today().isoformat(),))
+    elif stock_filter == 'new_closing_lows':
+        cursor.execute("""
+        SELECT * FROM (
+        SELECT symbol,name,stock_id,min(close),date
+        from stock_price join stock on stock.id = stock_price.stock_id
+        group by stock_id order by symbol
+        ) where date = ?
+        """, (date.today().isoformat(),))
+    else:
+        cursor.execute(""" SELECT id,symbol,name FROM stock order by symbol """)
     rows=  cursor.fetchall()
     # This is in JSON format
     return templates.TemplateResponse("index.html", {"request": request, "stocks": rows})
@@ -23,6 +44,12 @@ def index(request:Request,symbol):
     connection = sqlite3.connect("app.db")
     connection.row_factory = sqlite3.Row
     cursor=connection.cursor()
+
+    cursor.execute("""
+    SELECT * FROM strategy
+    """)
+    strategies = cursor.fetchall()
+
     cursor.execute(""" SELECT id,symbol,name FROM stock WHERE symbol = ? """,(symbol,))
     row =  cursor.fetchone()
     # This is in JSON format
@@ -32,4 +59,4 @@ def index(request:Request,symbol):
     """,(row['id'],))
     prices = cursor.fetchall()
 
-    return templates.TemplateResponse("stock_detail.html", {"request": request, "stock": row, "bars":prices})
+    return templates.TemplateResponse("stock_detail.html", {"request": request, "stock": row, "bars":prices, "strategies":strategies})
